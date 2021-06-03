3cf13fb2aeb07f44c3fc9bc98643d10e5ee65918
Arshavir Gasparyan <arshavir2908@gmail.com>
2021-06-03 11:33:36 +0400

    feat: find minimum element

---
+++ minPosElement.js
@@ -0,0 +1,18 @@
+function findElement(arr) {
+  function iter(iArr, minimum) {
+    if (iArr.length === 0 && minimum !== Infinity) {
+      return minimum;
+    }
+    if (iArr.length === 0 && minimum === Infinity) {
+      return -1;
+    }
+    const [first, ...rest] = iArr;
+    if (first < minimum && first >= 0) {
+      minimum = first;
+    }
+    return iter(rest, minimum);
+  }
+  return iter(arr, Infinity);
+}
+let arr = [-45, -9, 15, -5, -78];
+console.log(findElement(arr));
